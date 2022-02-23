8b21d49d13fdcd7702e4afee49ca82d5d2f50a69
Sanjay1269 <sanj.sanjay976@gmail.com>
2022-02-23 09:48:10 +0530

    crud-app

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='container'>
+            <div className='py-4'>
+                <h1>About page</h1>
+                <p className='lead'>While there are so many React UI libraries to choose from, React Bootstrap remains a popular choice because of its similarity with traditional Bootstrap and the availability of several Bootstrap themes. It is one of the fastest ways to build interfaces using React and Bootstrap.</p>
+                <p className='lead'>While there are so many React UI libraries to choose from, React Bootstrap remains a popular choice because of its similarity with traditional Bootstrap and the availability of several Bootstrap themes. It is one of the fastest ways to build interfaces using React and Bootstrap.</p>
+                <p className='lead'>While there are so many React UI libraries to choose from, React Bootstrap remains a popular choice because of its similarity with traditional Bootstrap and the availability of several Bootstrap themes. It is one of the fastest ways to build interfaces using React and Bootstrap.</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
